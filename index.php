bf728178ab2e9e5ebf2492b03b9856be8a2e261c
Harrison Louis Greeves <hgreeves@protonmail.com>
2020-09-20 18:24:47 +0800

    Added Google Analytics tag.

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
   <html lang="en">
     <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-178486694-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-178486694-1');
+    </script>
+
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -296,10 +306,10 @@
             $headers .= 'From: Message Request <$email>';
 
             if (!mail($to, $subject, $body, $headers)) {
-              echo "<div class='alert alert-danger'>Sorry. Something went wrong :(<br></div>";
+              echo "<div style='margin-bottom: 60px;' class='alert alert-danger'>Sorry. Something went wrong :(</div>";
             }
             else
-              echo '<div class="alert alert-success">Message sent. I will get back to you ASAP :)<br></div>';
+              echo "<div style='margin-bottom: 60px;' class='alert alert-success'>Message sent. I will get back to you ASAP :)</div>";
           }   
         ?>
                     
